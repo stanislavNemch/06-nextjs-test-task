aa9f9d64a0d17dc4438b6e7240595795ecf84f67
Stanislav Nemchenko <stas.buka@gmail.com>
2025-09-22 15:08:37 +0300

    feat: implement Header component and integrate navigation; add API for fetching notes

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,10 +1,13 @@
-const Notes = () => {
-    return (
-        <div>
-            <h1>User Notes</h1>
-            <p>This is the notes page of the user.</p>
-        </div>
-    );
+// 1. Імпортуємо функцію
+import { getNotes } from "@/lib/api";
+
+// 2. Робимо фукнцію асинхронною
+const Notes = async () => {
+    // 3. Виконуємо запит
+    const notes = await getNotes();
+    console.log("notes", notes);
+
+    return <div>Notes page</div>;
 };
 
 export default Notes;
